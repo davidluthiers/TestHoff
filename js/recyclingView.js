7755490e53793cef109e3f57964b5f4d98a8c7d8
davidluthiers <deepfeeling@hotmail.es>
2016-06-08 17:17:19 +0200

    typo

--- js/recyclingView.js
+++ js/recyclingView.js
@@ -155,7 +155,7 @@ define([
 					}
 					
 					if(keyflag)
-						this.router.drupaldo(this.createMedia.bind(this),this.history.get("languages").get("audioName"))
+						this.router.drupaldo(this.createMedia.bind(this),this.history.get("languages").get("audioName"));
 					//window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory+"audios/"+selfR.history.get("languages").get("audioName"), this.nodownload.bind(this), this.router.drupaldo(this.createMedia.bind(this),this.history.get("languages").get("audioName")));
 					//window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory+"audios/"+this.model.get("audioName"), this.nodownload.bind(this), this.cagao.bind(this));
 					
