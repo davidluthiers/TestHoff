421048d98154cf00db24c0bc557ebf54da17bb3e
davidluthiers <deepfeeling@hotmail.es>
2016-08-05 19:43:34 +0200

    fix y quito debugs

--- js/passwordView.js
+++ js/passwordView.js
@@ -10,7 +10,7 @@ define([
     'text!../Templates/headerandpanel.html',
     'jquerymobile'
   
-    ], function($, _, Backbone, XORCipher, passwordP, headerandpanel){
+    ], function($, _, Backbone, passwordP, headerandpanel){
 
 
 
@@ -34,16 +34,16 @@ define([
 			
                 }
 		
-				console.log("PassV 2");
+		
                 var self=this;
                 historycollection.get("languages").set("helppanel",self.history.get("languages").get("dic_password_protect")); //Aquí en lugar de cadena vacía iría el texto de panel de ayuda de la página password_protect 
-				console.log("PassV 2.1");
+			
                 result= historycollection.get("languages").toJSON();
-				console.log("PassV 2.1.1");
+	
                 compiledheaderandpanel=_.template( headerandpanel );
-				console.log("PassV 2.2");
+	
                 this.$el.empty().append(compiledTemplate(result)).append(compiledheaderandpanel(result));
-				console.log("PassV 2.3");
+	
 	
 	
 	
