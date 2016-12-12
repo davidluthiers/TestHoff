15a5406b516bee0305da8f4e06f9f5099099e628
davidluthiers <deepfeeling@hotmail.es>
2016-12-12 22:38:26 +0100

    fix

--- js/Router.js
+++ js/Router.js
@@ -635,8 +635,7 @@ define([
 				if(historial.get("profile").get("pass")!="" && $(".page").attr("style") != "display: none;"){
 					console.log(historial.get("profile").get("info"));
 					console.log(typeof historial.get("profile").get("info"));
-					console.log(historial.get("profile").get("info").substring(0,2));
-					if(historial.get("profile").get("info")!="facebook" && historial.get("profile").get("info").substring(0,2) != "20"){
+					if(historial.get("profile").get("info")!="facebook" && typeof historial.get("profile").get("info") != "number"){
 						console.log("ask for password");
 						//Tapar pantalla #password_protect
 						$(".page").attr("style","display: none;");
