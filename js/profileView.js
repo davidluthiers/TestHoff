8cbdd4d9c0e6f4ac6c442e40db3d46148382cc75
davidluthiers <deepfeeling@hotmail.es>
2017-01-23 18:38:29 +0100

    fix

--- js/profileView.js
+++ js/profileView.js
@@ -1185,7 +1185,7 @@ define([
 				this.history.create(profile);
 			},
 			
-			validateEmail: function(){
+			validateEmail: function(email){
 				var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 				return re.test(email);
 			},
