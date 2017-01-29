5728be8b38b1ba1597229b4ed057b03a2145ec76
davidluthiers <deepfeeling@hotmail.es>
2017-01-29 07:12:21 +0100

    Revert "debug"
    This reverts commit 1bbd50fffb5d3e41ee7e520f6ebd6e6970fc445c.

--- js/profileView.js
+++ js/profileView.js
@@ -557,9 +557,6 @@ define([
 						console.log(data);
 						data.push(historial.get("profile"));
 						data[data.length].userID = "";
-						console.log("After data2: ");
-						console.log(data);
-						console.log(data[data.length]);
 						for (index = 0; index < data.length; ++index) {
 							
                             var auxprofile = data[index];
