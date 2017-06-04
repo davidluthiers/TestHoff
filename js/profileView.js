d6a51e34ab5b4c87b8e41d00ae58c47e2626bb48
davidluthiers <deepfeeling@hotmail.es>
2017-06-04 19:11:33 +0200

    Revert "Test sin thumb"
    This reverts commit 25a09bede096905482351af6f48d70ad0ec825aa.

--- js/profileView.js
+++ js/profileView.js
@@ -750,7 +750,7 @@ define([
 								//var pictureurl;
 								try{
 									if(typeof auxprofile.picturevalue_thumb != 'undefined' && auxprofile.picturevalue_thumb != ""){
-										picturenewurl = auxprofile.picturevalue;
+										picturenewurl = auxprofile.picturevalue_thumb;
 									}
 									else{
 										picturenewurl="http://i.imgur.com/Tz9JNSg.png";
