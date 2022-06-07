53dbfc323ab5fc952171f3900e39e408c91939e4
davidluthiers <deepfeeling@hotmail.es>
2022-06-07 16:51:17 +0200

    Debug

--- www/js/summaryView.js
+++ www/js/summaryView.js
@@ -15,54 +15,59 @@ define([
         summaryView = Backbone.View.extend({
    
             render: function(historycollection){
-                this.$el.attr('data-role', 'page');
-                this.$el.attr('data-theme', 'a');
-                this.$el.attr('class', 'page');
+				try{
+					this.$el.attr('data-role', 'page');
+					this.$el.attr('data-theme', 'a');
+					this.$el.attr('class', 'page');
 
-                compiledTemplate = _.template( summary );
-	
-                this.collection=historycollection;
-	
-                var self=this;
-                historycollection.get("languages").set("helppanel","");
-                result= historycollection.get("languages").toJSON();
-                compiledheaderandpanel=_.template( headerandpanel );
-                this.$el.empty().append(compiledTemplate(result)).append(compiledheaderandpanel(result));
-	
-                this.$(".dic_help").hide();
-                this.visioncount=0;
-                this.feedcount=0;
-				console.log("colección");
-                console.log(this.collection);
-                //console.log(self.collection.reverse());
-                if(self.collection.length>0)
-                    for(iter=self.collection.length-1;iter>=0;iter--){
-                        this.takefeed(self.collection.at(iter));
-                    }
-                //self.collection.forEach(this.takefeed, this);
-                //arreglos por el orden invertido
-                if(this.visioncount>0)//hay vision boards
-                    this.$("#summarylist").prepend(" <li class='visionboard separadortool' data-role='list-divider' role='heading'>" + historycollection.get("languages").get("dic_vision_header") + "</li>");
-                this.$("#listasummarytools").prepend("<li class='recycling separadortool' data-role='list-divider' role='heading'>" + historycollection.get("languages").get("dic_summary_text1") + "</li>");
-	
-                if(this.visioncount==0) this.$('#summarylist').hide();
-                if(this.feedcount==0) this.$('#listasummarytools').hide();
-				
-				try{ //Seguro contra spinners colgados
-					window.plugins.spinnerDialog.hide();
-					setTimeout(function(){
-						try{
-							navigator.splashscreen.hide();
+					compiledTemplate = _.template( summary );
+		
+					this.collection=historycollection;
+		
+					var self=this;
+					historycollection.get("languages").set("helppanel","");
+					result= historycollection.get("languages").toJSON();
+					compiledheaderandpanel=_.template( headerandpanel );
+					this.$el.empty().append(compiledTemplate(result)).append(compiledheaderandpanel(result));
+		
+					this.$(".dic_help").hide();
+					this.visioncount=0;
+					this.feedcount=0;
+					console.log("colección");
+					console.log(this.collection);
+					//console.log(self.collection.reverse());
+					if(self.collection.length>0)
+						for(iter=self.collection.length-1;iter>=0;iter--){
+							this.takefeed(self.collection.at(iter));
 						}
-						catch(e){
-							console.log(e);
-						}
-					},2000);
+					//self.collection.forEach(this.takefeed, this);
+					//arreglos por el orden invertido
+					if(this.visioncount>0)//hay vision boards
+						this.$("#summarylist").prepend(" <li class='visionboard separadortool' data-role='list-divider' role='heading'>" + historycollection.get("languages").get("dic_vision_header") + "</li>");
+					this.$("#listasummarytools").prepend("<li class='recycling separadortool' data-role='list-divider' role='heading'>" + historycollection.get("languages").get("dic_summary_text1") + "</li>");
+		
+					if(this.visioncount==0) this.$('#summarylist').hide();
+					if(this.feedcount==0) this.$('#listasummarytools').hide();
+					
+					try{ //Seguro contra spinners colgados
+						window.plugins.spinnerDialog.hide();
+						setTimeout(function(){
+							try{
+								navigator.splashscreen.hide();
+							}
+							catch(e){
+								console.log(e);
+							}
+						},2000);
+					}
+					catch(e){
+						console.log(e);
+					}
 				}
 				catch(e){
-					console.log(e);
-				}
-
+						console.log("Error en summary.render:");
+						console.log(e);
+					}
 	
             },
 	
